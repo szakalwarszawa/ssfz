faecce436c2d530361f0169d984489b8fa25e608
Stefan Kania <luklub@o2.pl>
2019-06-01 01:34:51 +0200

    Poprawki w DocBlock (naprawia #82326)

--- src/Parp/SsfzBundle/Twig/AppExtension.php
+++ src/Parp/SsfzBundle/Twig/AppExtension.php
@@ -4,23 +4,46 @@ namespace Parp\SsfzBundle\Twig;
 
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use HTMLPurifier;
 
+/**
+ * Dodaje specyficzne dla aplikacji rozszerzenia systemu szablonów Twig.
+ */
 class AppExtension extends AbstractExtension
 {
+    /**
+     * @var HTMLPurifier
+     */
     private $htmlPurifier;
 
+    /**
+     * Konstruktor.
+     */
     public function __construct()
     {
-        $config = \HTMLPurifier_Config::createDefault();
-        $this->htmlPurifier = new \HTMLPurifier($config);
-    }
-    public function getFilters()
-    {
-        return array(
-            new TwigFilter('purify', array($this, 'purify')),
-        );
+        $config = HTMLPurifier_Config::createDefault();
+        $this->htmlPurifier = new HTMLPurifier($config);
     }
 
+    /**
+     * Zwraca wykaz filtrów dodanych do systemu szablonów.
+     *
+     * @return array|TwigFiler[]
+     */
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('purify', [$this, 'purify']),
+        ];
+    }
+
+    /**
+     * Wykomuje filtr "purify".
+     *
+     * @param string $string
+     *
+     * @return string
+     */
     public function purify($string)
     {
         return $this->htmlPurifier->purify($string);
