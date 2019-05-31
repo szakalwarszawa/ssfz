dbda4acef4f41363ab69f757280f9301c9679738
marek_korecki <marek_korecki@parp.gov.pl>
2019-05-31 13:56:10 +0200

    dodanie path przy rejestracji

--- src/Parp/SsfzBundle/Controller/UzytkownikController.php
+++ src/Parp/SsfzBundle/Controller/UzytkownikController.php
@@ -25,7 +25,7 @@ class UzytkownikController extends Controller {
      * 
      * @param Request $request
      * 
-     * @Route("/rejestracja")
+     * @Route("/rejestracja", name="rejestracja_konta_beneficjenta")
      * 
      * @return Response
      */
