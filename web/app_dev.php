1bb8c46da6c7b8b64ab5dcf5ff76412eb47a7453
Łukasz Lubliński <lukasz_lublinski@parp.gov.pl>
2019-06-03 18:45:38 +0200

    Poprawki czytelności kodu powiązane z zaleceniem 1.5.1.2

--- web/app_dev.php
+++ web/app_dev.php
@@ -1,48 +1,60 @@
 <?php
-//    die();
-// Sprawdzenie czy adres uzytkownika (lub jego czesc) jest na liscie dozwolonych IP.
-$ipDozwolone = array(
+
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Lista zaufanych adresów IP, dla których dostępny jest tryb deweloperski aplikacji.
+ *
+ * @var string[]
+ */
+$ipDozwolone = [
        '10.10.120.',  // VLAN BI.
        '10.10.225.',  // VPN Admin FortiClient
        '10.10.50.',   // VPN Admin IPSec
        '127.0.0.1'    // localhost
-);
+];
+
+/**
+ * Sprawdza, czy adres, z którgo przyszło żądanie jest na liście zaufanych adresów IP.
+ *
+ * @param string $ip
+ * @param string $remoteAddr
+ *
+ * @return bool
+ */
+function czyIpJestDozwolony($ip = null, $remoteAddr = null)
+{
+    $ip = (string) $ip;
+    $remoteAddr = (string) $remoteAddr;
+    $wycinek = substr($remoteAddr, 0, strlen($ip));
+    $wynik = ($wycinek === $ip) ? true : false;
+
+    return $wynik;
+}
 
 $remoteAddr = @$_SERVER['REMOTE_ADDR'];
 if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
     $remoteAddr = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
 }
 
-function czyIpJestDozwolony($ip = null, $remoteAddr = null)
-{
-       $ip = (string) $ip;
-       $remoteAddr = (string) $remoteAddr;
-       $wycinek = substr($remoteAddr, 0, strlen($ip));
-       $wynik = ($wycinek === $ip) ? true : false;
-       return $wynik;
-}
 $dostepZabroniony = true;
 foreach ($ipDozwolone as $ip) {
-       if (true === czyIpJestDozwolony($ip, $remoteAddr)) {
-               $dostepZabroniony = false;
-               break;
-       }
+    if (true === czyIpJestDozwolony($ip, $remoteAddr)) {
+        $dostepZabroniony = false;
+        break;
+    }
 }
 if (true === $dostepZabroniony) {
-       header('HTTP/1.0 403 Forbidden');
-       exit('Brak uprawnień do korzystania z zasobu.');
+    header('HTTP/1.0 403 Forbidden');
+    exit('Brak uprawnień do korzystania z zasobu.');
 }
 // Koniec sprawdzania IP.
-
-
-use Symfony\Component\Debug\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read https://symfony.com/doc/current/setup.html#checking-symfony-application-configuration-and-setup
 // for more information
 umask(0002);
-
 
 require __DIR__.'/../app/autoload.php';
 Debug::enable();
