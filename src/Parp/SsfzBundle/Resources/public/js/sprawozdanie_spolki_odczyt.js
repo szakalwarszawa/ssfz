fdd9b12e0d2a88c06ace31d4eab6a0452032eec2
Łukasz Lubliński <lukasz_lublinski@parp.gov.pl>
2019-07-18 12:31:28 +0200

    Poprawione formatowania na szablonach do odczytu

---
+++ src/Parp/SsfzBundle/Resources/public/js/sprawozdanie_spolki_odczyt.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    var plnOptions = {
+        symbol: ' zł',
+        precision: 2,
+        decinal: '.',
+        thousand: ' ',
+        format: "%v%s"
+    },
+    percentOptions = {
+        symbol: ' %',
+        precision: 2,
+        decinal: '.',
+        thousand: ' ',
+        format: "%v%s"
+    };
+   
+    function format() {
+        $('.decimal-11-2').each(function () {
+            var txt;
+
+            txt = $(this).text();
+            txt = accounting.toFixed(txt, 2);
+            txt = accounting.formatMoney(txt, plnOptions);
+            $(this).text(txt);
+        });
+
+        $('.percent-11-2').each(function () {
+            var txt;
+
+            txt = $(this).text();
+            txt = accounting.toFixed(txt, 2);
+            txt = accounting.formatMoney(txt, percentOptions);
+            $(this).text(txt);
+        });
+    }
+
+    format();
+});
