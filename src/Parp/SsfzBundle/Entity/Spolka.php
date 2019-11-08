71612c1696bf9f51a381ff97fea4248aa052ca8c
hubertuspp <hubertuspp@gmail.com>
2019-11-08 17:57:49 +0100

    psr

--- src/Parp/SsfzBundle/Entity/Spolka.php
+++ src/Parp/SsfzBundle/Entity/Spolka.php
@@ -832,11 +832,11 @@ class Spolka
                 ''
             );
             if ((float)number_format(
-                    $this->getKwInwestycji(),
-                    2,
-                    '.',
-                    ''
-                ) !== (float)$sum
+                $this->getKwInwestycji(),
+                2,
+                '.',
+                ''
+            ) !== (float)$sum
             ) {
                 $context->buildViolation('Suma kwot ze środków wsparcia i środków prywatnych musi równać'
                     . ' się kwocie inwestycji.')
