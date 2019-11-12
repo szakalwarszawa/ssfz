6f2b4a4256451c7b47170ee84d9cdd50a1078d5e
hubertuspp <hubertuspp@gmail.com>
2019-11-12 18:38:29 +0100

    literowka

--- setup/migrations/Version20191112170556.php
+++ setup/migrations/Version20191112170556.php
@@ -15,7 +15,7 @@ class Version20191112170556 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $this->addSql('ALTER TABLE sfz_uzytkownik ADD salt VARCHAR(255) DEFAULT NULL';
+        $this->addSql('ALTER TABLE sfz_uzytkownik ADD salt VARCHAR(255) DEFAULT NULL');
     }
 
     /**
