d01d8c8a0b3fe3e5c335886eee39cac4dd646ee1
Łukasz Lubliński <lukasz_lublinski@parp.gov.pl>
2019-11-13 17:50:02 +0100

    Poprawione deklaracje zwracanego typu.

--- src/Parp/SsfzBundle/Controller/SecurityController.php
+++ src/Parp/SsfzBundle/Controller/SecurityController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\{
     Request,
+    Response,
     RedirectResponse
 };
 
